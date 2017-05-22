4e37d3380c05b24b1931dafcd13f1420b44ac264
Israel <israel.santanna@gmail.com>
2017-05-21 21:03:50 -0300

    Fix blob file not being destroyed after worker use.

--- src/createDisposableWorker.js
+++ src/createDisposableWorker.js
@@ -4,7 +4,10 @@ export const createDisposableWorker = response => {
   const worker = new Worker(URL.createObjectURL(blob)) // eslint-disable-line
   worker.post = message =>
     new Promise((resolve, reject) => {
-      worker.onmessage = event => resolve(event.data)
+      worker.onmessage = event => {
+        URL.revokeObjectURL(blob)
+        resolve(event.data)
+      }
       worker.onerror = e => {
         console.error(`Error: Line ${e.lineno} in ${e.filename}: ${e.message}`)
         reject(e)
