b2168a5f2617d97e5842a7ec14952fe660e197be
Israel <israel.santanna@gmail.com>
2017-05-21 21:04:42 -0300

    Removed unused code

--- src/tests/unit/ModuleCreate/moduleCreate.test.js
+++ src/tests/unit/ModuleCreate/moduleCreate.test.js
@@ -1,9 +1,6 @@
-/* global jest */
 import CorrectCases from './CorrectCases'
 import WrongCases from './WrongCases'
 import { create } from '../../../create'
-
-jest.spyOn(console, 'error')
 
 CorrectCases(create)
 WrongCases(create)
