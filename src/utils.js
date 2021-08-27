ad48014660248de05c9e492ee1b6c1e589d6a797
Israel Sant'Anna <israel.santanna@gmail.com>
2021-08-27 14:46:52 -0300

    Update logic to capture circular structure errors

--- src/utils.js
+++ src/utils.js
@@ -51,7 +51,9 @@ const argumentError = ({ expected = '', received, extraInfo = '' }) => {
   try {
     return new TypeError(`${'You should provide ' + expected}${'\n' + extraInfo}${'\nReceived: ' + JSON.stringify(received)}`)
   } catch (err) {
-    if (err.message === 'Converting circular structure to JSON') {
+    if (err.message === `Converting circular structure to JSON
+    --> starting at object with constructor 'Object'
+    --- property 'received' closes the circle`) {
       return new TypeError(`${'You should provide ' + expected}${'\n' + extraInfo}${'\nReceived a circular structure: ' + received}`)
     }
     throw err
