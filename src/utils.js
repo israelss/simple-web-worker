00585dfd21e86a4b4ad449f21c32e92ade167bbf
BluuArc <joshua.castor@joshuacastor.me>
2021-08-28 18:09:35 -0500

    Update error message check in argumentError to account for change in error message when converting a circular structure to JSON

--- src/utils.js
+++ src/utils.js
@@ -51,7 +51,7 @@ const argumentError = ({ expected = '', received, extraInfo = '' }) => {
   try {
     return new TypeError(`${'You should provide ' + expected}${'\n' + extraInfo}${'\nReceived: ' + JSON.stringify(received)}`)
   } catch (err) {
-    if (err.message === 'Converting circular structure to JSON') {
+    if (err.message.indexOf('Converting circular structure to JSON') > -1) {
       return new TypeError(`${'You should provide ' + expected}${'\n' + extraInfo}${'\nReceived a circular structure: ' + received}`)
     }
     throw err
