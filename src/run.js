bd7685b6904e03e7858f392f414933665410515c
yuanpengfei03 <yuanpengfei03@meituan.com>
2022-02-09 18:23:31 +0800

    feature(async): support async func in action and manual to close child worker

--- src/run.js
+++ src/run.js
@@ -1,11 +1,11 @@
-import { argumentError, isValid, makeResponse } from './utils'
+import { argumentError, isValid, isAsyncFunc, makeResponse, makeManualCloseResponse } from './utils'
 import { createDisposableWorker } from './createDisposableWorker'
 
 export const run = (work = null, args) => {
   const validWork = isValid(work)('function')
   const validArgs = isValid(args)(['array', 'undefined'])
   if (validWork && validArgs) {
-    const worker = createDisposableWorker(makeResponse(work))
+    const worker = isAsyncFunc(work) ? createDisposableWorker(makeManualCloseResponse(work), isAsyncFunc) : createDisposableWorker(makeResponse(work))
     return worker.post({ args })
   }
   if (!validWork) console.error(argumentError({ expected: 'a function', received: work }))
